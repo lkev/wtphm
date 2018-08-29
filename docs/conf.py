250322b045e1defa6a19fb74d008cc21c25bfd9c
lkev <leahykev@gmail.com>
2018-08-29 17:21:23 +0100

    docs changes

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,14 @@
 #
 import os
 import sys
+import mock
+
 sys.path.insert(0, os.path.abspath('../'))
+
+MOCK_MODULES = [
+    'numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
 
 # sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
 

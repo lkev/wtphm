7dc4245bdda8deceeb2722d082e820fbd6c4a3b5
Kevin Leahy <leahykev@gmail.com>
2020-03-27 23:42:48 +0000

    changed files to get docs building

--- docs/conf.py
+++ docs/conf.py
@@ -61,3 +61,9 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# The below are the main requirements of the project. They are not actually
+# needed to build the docs so are saved here as fake packages.
+autodoc_mock_imports = [
+    "matplotlib", "numpy", "sklearn", "pandas", "operator", "warning"]

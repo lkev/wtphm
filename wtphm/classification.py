32b473eb3efe55ba99c00bccbcd0090c889fefc3
Kevin Leahy <leahykev@gmail.com>
2020-03-30 19:27:57 +0100

    added examples (again!)

--- wtphm/classification.py
+++ wtphm/classification.py
@@ -1,5 +1,5 @@
-# from sklearn.base import TransformerMixin
-# from .batch_clustering import investigate_label
+import numpy as np
+from scipy.ndimage.interpolation import shift
 import pandas as pd
 
 
@@ -165,3 +165,50 @@ def _get_pre_stop_ids(fault_batches, scada_data_l, pre_stop_lims):
         pre_stop_ids = pre_stop_ids.append(cur_pre_stop_ids)
         pre_stop_batch_ids[b.Index] = cur_pre_stop_batch_ids
     return pre_stop_ids, pre_stop_batch_ids
+
+
+def get_lagged_features(X, y, features_to_lag_inds, steps):
+    """
+    Returns an array with certain columns as lagged features for classification
+
+    Args
+    ----
+    X: m*n np.ndarray
+        The input features, with m samples and n features
+    y: m*1 np.ndarray
+        The m target values
+    features_to_lag_inds: np.array
+        The indices of the columns in ``X`` which will be lagged
+    steps: int
+        The number of lagging steps. This means for feature 'B' at time T,
+        features will be added to X at T for B@(T-1), B@(T-2)...B@(T-steps).
+
+    Returns
+    -------
+    X_lagged: np.ndarray
+        An array with the original features and lagged features appended. The
+        number of samples will necessarily be decreased because there will be
+        some samples at the start with NA values for features.
+    y_lagged: np.ndarray
+        An updated array of target vaues corresponding to the new number of
+        samples in ``X_lagged``
+
+    """
+    # get a slice with columns of features to be lagged
+    X_f = X[:, features_to_lag_inds]
+
+    m = X_f.shape[0]
+    n = X_f.shape[1]
+    n_ = n * steps
+
+    X_lagged = np.zeros((m, n_))
+
+    for i in np.arange(0, steps):
+        X_lagged[:, i * n:(i * n) + n] = shift(X_f, [i + 1, 0], cval=np.NaN)
+
+    X_lagged = np.concatenate((X_f, X_lagged), axis=1)
+
+    y_lagged = y[~np.isnan(X_lagged).any(axis=1)]
+    X_lagged = X_lagged[~np.isnan(X_lagged).any(axis=1)]
+
+    return X_lagged, y_lagged
